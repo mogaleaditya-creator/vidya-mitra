1e778b8ce87dfe9f1373927d7cf7c075790747da
ramzan <ramjan.magdum8359@gmail.com>
2026-05-02 10:11:01 +0530

    Intial Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return "This is about page"
+
+@app.route("/contact")
+def contact():
+    return "Contact page"
+
+if __name__ == "__main__":
+    app.run(debug=True)
